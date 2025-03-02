9037dc24c9a1a13506dde0f19c831353661a8abb
JaiBhargav <bhargav.cloud01@gmail.com>
2025-03-02 10:58:53 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,3 +8,8 @@ sudo apt install -y apache2 unzip
 
 # Stop Apache service before deploying new files
 sudo systemctl stop apache2
+
+
+# Remove existing deployment files to avoid conflicts
+sudo rm -rf /var/www/html/src/pages
+
